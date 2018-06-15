1d2eb8b6b94e778e38bca8a6f5a06b421d3ba056
Marcus Kopp <marcus.kopp86@gmail.com>
2018-06-15 17:08:12 +0200

    Updates work, but no persistent website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0, fruits: { id: 0, value: 'avocado'} };
+  }
+
+  tick() {
+    const newFruit = this.state.fruits;
+    const secondPassed = this.state.seconds + 1;
+    newFruit[secondPassed] = 'avocado';
+    this.setState(() => ({
+      seconds: secondPassed,
+      fruits: newFruit
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="component-wrapper">
+        <FruitList fruits={this.state.fruits} />
+      </div>
+    );
+  }
+}
+
+class FruitList extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <ul className="list-group text-center">
+          {Object.entries(this.props.fruits).map(
+            ([key,value]) => {
+              return (
+                <li>{key} : {value}</li>
+              );
+            },
+          )}
+        </ul>
       </div>
     );
   }
